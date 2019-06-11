76c4472bb218cc10eb04286b54a73537489b5c1a
Christian <chr.vadala@gmail.com>
2019-06-11 22:39:02 +0200

    Updates

---
+++ precache-manifest.6b109804009d568cb93e4d158f4b13ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c335a5d96a6063f4469447aa9405f4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb8ca91cc98e02b74fdc",
+    "url": "/static/css/2.c96fd6d6.chunk.css"
+  },
+  {
+    "revision": "4a1ef7301031088692a9",
+    "url": "/static/css/main.74f5821c.chunk.css"
+  },
+  {
+    "revision": "cb8ca91cc98e02b74fdc",
+    "url": "/static/js/2.d921c0f4.chunk.js"
+  },
+  {
+    "revision": "4a1ef7301031088692a9",
+    "url": "/static/js/main.ec28718d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "5490899417d517483d6098765da1c447",
+    "url": "/static/media/avatar.54908994.png"
+  }
+]);
