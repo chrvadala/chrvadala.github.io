107690de743bac6fe7db1e91f2a3d2d707577a47
Christian <chr.vadala@gmail.com>
2021-03-21 19:21:04 +0100

    improves page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,9 +4,12 @@ import styles from './Projects.module.css'
 
 export default function Projects({projects}) {
   return (
-    <div className={styles.grid}>
-      {projects.map(project => <ProjectCard key={project.id} project={project}/>)}
-    </div>
+    <>
+      <h3>My Open Source Projects</h3>
+      <div className={styles.grid}>
+        {projects.map(project => <ProjectCard key={project.id} project={project}/>)}
+      </div>
+    </>
   )
 }
 
