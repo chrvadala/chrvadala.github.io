fedab62dc9e81e122bb674e7d32f63aea89c58a2
Christian Vadala <chr.vadala@gmail.com>
2024-08-19 18:12:35 +0200

    fixes fontawesome css loading

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
+/* fontawesome setup */
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import ProjectsList from '../components/ProjectsList';
